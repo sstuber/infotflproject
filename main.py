9ec94397c91a764d3d051b5edc192699eb1bacb6
DESKTOP-DPQU3DT\Tianyi <t.chen@students.uu.nl>
2018-10-22 17:00:30 +0200

    Small spelling fix!

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from function_finder import find_more_or_less
 # The tourists are accommodated in 3 hotels. There are 8 more tourists in hotel 2 than in hotel 1 and in hotel 3 there are 14 more tourists than in hotel 2. If there is a total of 258 tourists, how many tourists are accommodated in each of the hotels ?
 
 def main():
-    input_sentence = input('Enter senntence\n')
+    input_sentence = input('Enter sentence\n')
 
     equation_type = extract_equation_type_from_sentence(input_sentence)
 
