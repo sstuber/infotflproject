9bd0485ddbcd87475c77217f273c44dc4421897e
Stan <stanvanmeerendonk@gmail.com>
2018-10-25 20:47:01 +0200

    Add multiplication problem

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from purchasing_problem import PurchasingProblem
 from stanfordcorenlp import StanfordCoreNLP
 from function_finder import find_more_or_less
 from substraction_problem import SubstractionProblem
+from multiplication_problem import MultiplicationProblem
 
 # The tourists are accommodated in 3 hotels. There are 8 less tourists in hotel 2 than in hotel 1 and in hotel 3 there are 14 more tourists than in hotel 2. If there is a total of 258 tourists, how many tourists are accommodated in each of the hotels ?
 
@@ -39,6 +40,10 @@ def main():
 
     if equation_type == 'division':
         problem = DivisionProblem(input_sentence, nlp)
+        problem.print_problem()
+
+    if equation_type == 'multiplication':
+        problem = MultiplicationProblem(input_sentence, nlp)
         problem.print_problem()
 
     if equation_type == 'purchasing':
---
+++ multiplication_problem.py
@@ -0,0 +1,22 @@
+
+from variable_finder import find_variables
+
+
+class MultiplicationProblem:
+
+    def __init__(self,sentence, nlp=None):
+        self.sentence = sentence
+        self.variables = find_variables(sentence, nlp)
+
+    def print_problem(self):
+        x = self.variables[0]
+        y = self.variables[1]
+        z = self.variables[2]
+
+        for variable in self.variables:
+            print(variable)
+
+        print(f'Solution: solve {x.symbol} * {y.symbol} = {z.symbol}')
+
+
+
